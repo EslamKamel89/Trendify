f941f7b8c7dd59c97c1cf5c6453abcd72f9e7aaf
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-19 09:17:55 +0300

    feat: add @vueuse/nuxt module to project dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  modules: ["shadcn-nuxt"],
+  modules: ["shadcn-nuxt", "@vueuse/nuxt"],
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
