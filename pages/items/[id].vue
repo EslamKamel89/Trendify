bc2ed16d88da4a0140c80dfa18bf54f1291f56ef
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-21 15:09:13 +0300

    feat: add Product detail page with dynamic routing and image display

---
+++ pages/items/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const id = Number(useRoute().params.id);
+const product = computed(() => {
+  return products.filter((product) => product.id == id)[0];
+});
+</script>
+<template>
+  <div class="mx-auto w-full justify-between gap-4 lg:flex">
+    <div class="md:w-[40%]">
+      <div class="h-[300px] overflow-hidden rounded-lg">
+        <img
+          :src="product.url"
+          alt=""
+          class="h-[300px] object-cover transition-all duration-700 hover:scale-110"
+        />
+      </div>
+    </div>
+  </div>
+</template>
